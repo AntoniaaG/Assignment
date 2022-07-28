b047c0bda619ed5b33889b1b7ec7f0584e6a411a
AntoniaaG <antonia.ghilvacs@evozon.com>
2022-07-28 18:29:58 +0300

    assignment solution

---
+++ Assignment1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello \nAntonia!");
+            Console.WriteLine(2+3);
+            double num1 = 7, num2 = 4; 
+            Console.WriteLine(num1/num2);
+            Console.WriteLine(-5 + 8 * 6);
+            Console.WriteLine((55 + 9) % 9);
+            Console.WriteLine(20 + -3 * 5 / 8);
+            Console.WriteLine(5 + 15 / 3 * 2 - 8 % 3);
+        }
+    }
+}
